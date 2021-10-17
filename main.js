4b1d2a5e335445172226fc61f372deb427f8cbed
JahnieTurner <80420943+JahnieTurner@users.noreply.github.com>
2021-10-17 19:49:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 var canvas = new fabric.Canvas('myCanvas');
+
 block_y = 1;
 block_x = 1;
 
@@ -9,16 +10,19 @@ block_image_height = 430;
 var block_image_object = "";
 
 function new_image(get_image) {
+
 	fabric.Image.fromURL(get_image, function (Img) {
-		block_image_object.scaletoWidth(block_image_width)
-		block_image_object.scaletoHeight(block_image_height)
-		block_image_object.strike({
+		block_image_object = Img;
+		
+		block_image_object.scaleToWidth(block_image_width)
+		block_image_object.scaleToHeight(block_image_height)
+		block_image_object.set({
 			top: block_y,
 			left: block_x
 		});
-	}
+		canvas.add(block_image_object);
+	});
 }
-
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e) {
